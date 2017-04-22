7de59272e136b3f23dfa8b42bf400320931be7c9
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-23 00:30:50 +0200

    try to fix drop down

--- kidsmap_website/mapScript.js
+++ kidsmap_website/mapScript.js
@@ -86,9 +86,12 @@ function initialize() {
         var high = [151, 83, 34];   // color of largest datum
 
         var value = feature.getProperty(variable);
-
+        console.log('value is:');
+        console.log(value);
         var delta = (value - dataMin) / (dataMax - dataMin);
-
+        console.log('delta is:');
+        console.log(delta);
+        
         var color = [];
         for (var i = 0; i < 3; i++) {
           // calculate an integer color based on the delta
@@ -163,15 +166,15 @@ function initialize() {
 
     // if there is something selected on the drop-down box,
     // show the data-caret at the tile's value
-    var selectBox = document.getElementById('tile-variable');
-    var variable = selectBox.options[selectBox.selectedIndex].value;
-    if (variable != 'clear') {
-      var value = event.feature.getProperty(variable);
-      var delta = (value - dataMin) / (dataMax - dataMin);
-      caretPercent = delta * 100;
-      document.getElementById('data-caret').style.display = 'block';
-      document.getElementById('data-caret').style.paddingLeft = caretPercent + '%';
-    }
+    //var selectBox = document.getElementById('tile-variable');
+    //var variable = selectBox.options[selectBox.selectedIndex].value;
+    //if (variable != 'clear') {
+      //var value = event.feature.getProperty(variable);
+      //var delta = (value - dataMin) / (dataMax - dataMin);
+      //caretPercent = delta * 100;
+      //document.getElementById('data-caret').style.display = 'block';
+      //document.getElementById('data-caret').style.paddingLeft = caretPercent + '%';
+    //}
     
     var info = event.feature.getProperty('info').split(",");
     
