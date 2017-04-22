6e6d73e039f021e5fa0518d1894eafb310d2d5f2
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-23 00:54:44 +0200

    remove the caret

--- kidsmap_website/mapScript.js
+++ kidsmap_website/mapScript.js
@@ -159,24 +159,6 @@ function initialize() {
       title: event.feature.getProperty('obName')
     });
     markers.push(marker);
-
-    // if there is something selected on the drop-down box,
-    // show the data-caret at the tile's value
-    var selectBox = document.getElementById('tile-variable');
-    var variable = selectBox.options[selectBox.selectedIndex].value;
-    if (variable != 'clear') {
-      var value = event.feature.getProperty(variable);
-      console.log('value is');
-      console.log(value);
-      var delta = (value - dataMin) / (dataMax - dataMin);
-      console.log('delta is');
-      console.log(delta);
-      caretPercent = delta * 100; //make smaller
-      console.log('percent is');
-      console.log(caretPercent);
-      document.getElementById('data-caret').style.display = 'block';
-      document.getElementById('data-caret').style.paddingLeft = ((((event.feature.getProperty(variable)) - dataMin) / (dataMax - dataMin)) * 100) + '%';
-    }
     
     var info = event.feature.getProperty('info').split(",");
     
