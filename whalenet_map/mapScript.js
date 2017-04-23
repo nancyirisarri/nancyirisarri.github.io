b6a4faa4340e689f7f612659d9466f508d6dbbe8
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-23 16:04:48 +0200

    trial

--- whalenet_map/mapScript.js
+++ whalenet_map/mapScript.js
@@ -1,7 +1,9 @@
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: {lat: -33, lng: 151},
-          disableDefaultUI: true
-        });
-      }
+function initialize() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: -33, lng: 151},
+    disableDefaultUI: true
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
