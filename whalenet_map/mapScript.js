440c0d5f7737dee5e68c823c2f971af057f1f2c4
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-23 15:35:53 +0200

    trail and error on map javascript

--- whalenet_map/mapScript.js
+++ whalenet_map/mapScript.js
@@ -1,6 +1,21 @@
+var map;
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    zoom: 6,
-    center: {lat: -33.872, lng: 151.252},
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8,-187.3),
+    mapTypeId: 'terrain'
   });
+}
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+window.eqfeed_callback = function(results) {
+for (var i = 0; i < results.features.length; i++) {
+  var coords = results.features[i].geometry.coordinates;
+  var latLng = new google.maps.LatLng(coords[1],coords[0]);
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+}
 }
