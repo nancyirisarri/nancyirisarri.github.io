841f363ed1fde83a04ee54892223df3e919e758a
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-23 15:36:49 +0200

    Revert "trail and error on map javascript"
    This reverts commit 440c0d5f7737dee5e68c823c2f971af057f1f2c4.

--- whalenet_map/mapScript.js
+++ whalenet_map/mapScript.js
@@ -1,21 +1,6 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    zoom: 2,
-    center: new google.maps.LatLng(2.8,-187.3),
-    mapTypeId: 'terrain'
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 6,
+    center: {lat: -33.872, lng: 151.252},
   });
-}
-
-// Loop through the results array and place a marker for each
-// set of coordinates.
-window.eqfeed_callback = function(results) {
-for (var i = 0; i < results.features.length; i++) {
-  var coords = results.features[i].geometry.coordinates;
-  var latLng = new google.maps.LatLng(coords[1],coords[0]);
-  var marker = new google.maps.Marker({
-    position: latLng,
-    map: map
-  });
-}
 }
