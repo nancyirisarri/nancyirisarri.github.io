1279386151bca110047a35b1efab9f61186eaf70
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-23 15:39:43 +0200

    trial

--- whalenet_map/mapScript.js
+++ whalenet_map/mapScript.js
@@ -1,6 +1,14 @@
 function initMap() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    zoom: 6,
-    center: {lat: -33.872, lng: 151.252},
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
   });
 }
