1696fa63bf7105485693ef0cc232e8d4ad658ca8
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-23 18:40:19 +0200

    load data

--- whalenet_map/mapScript.js
+++ whalenet_map/mapScript.js
@@ -4,15 +4,11 @@ var map;
 function initialize() {
   var mapDiv = document.getElementById('map-canvas');
   map = new google.maps.Map(mapDiv, {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    zoom: 3,
+    center: new google.maps.LatLng(36.83776, -76.00996), 
   });
 
-  // We add a DOM event here to show an alert if the DIV containing the
-  // map is clicked.
-  google.maps.event.addDomListener(mapDiv, 'click', function() {
-    window.alert('Map was clicked!');
-  });
+  map.data.loadGeoJson('data.json');
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
