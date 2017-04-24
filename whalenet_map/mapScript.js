a4369c3cd82c2f08fd4996d4ab5155bfed70024c
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-24 16:30:44 +0200

    try out hybrid map

--- whalenet_map/mapScript.js
+++ whalenet_map/mapScript.js
@@ -22,7 +22,7 @@ function initialize() {
   map = new google.maps.Map(mapDiv, {
     zoom: 7,
     center: new google.maps.LatLng(40.233, -73.463), 
-    mapTypeId: 'satellite'
+    mapTypeId: 'hybrid'
   });
   
   infowindow = new google.maps.InfoWindow();
